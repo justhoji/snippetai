b8c0c0c615512c827ff016d65dfca2007a00db2d
Khojiakbar Khayitboev <xayitbaevhojiakbar@gmail.com>
2026-05-11 12:33:03 +0500

    refactor: remove unused React import from App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import Layout from "./components/Layout";
 import Header from "./components/Header";
 import SnippetList from "./components/SnippetList";
